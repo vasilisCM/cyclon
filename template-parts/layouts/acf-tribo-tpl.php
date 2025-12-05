9b7db6b1cda4fa4d2a7614670e20bfca86a95915
polinkar <pkaragiannopoulou@conceptmaniax.com>
2025-12-05 11:55:19 +0200

    menu

--- template-parts/layouts/acf-tribo-tpl.php
+++ template-parts/layouts/acf-tribo-tpl.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center">
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                 <div class="feature__Image">
-                    <img src="<?php echo get_sub_field('image'); ?>" class="img-fluid"/>
+                    <img src="<?php echo get_sub_field('image'); ?>" class="img-fluid" />
                 </div>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
@@ -20,7 +20,7 @@
                     </p>
 
                     <a class="mButton mButton--trans mleft"
-                       href="<?php echo get_sub_field('button_1_url'); ?>">
+                        href="<?php echo get_sub_field('button_1_url'); ?>">
                         <?php echo get_sub_field('button_1_text'); ?>
                     </a>
 
