be444b6b4c2e10121013e62697d1af58555ba6b4
vasilisCM <vpafiliaris@conceptmaniax.com>
2025-12-18 14:27:06 +0200

    categories tray backend

--- template-parts/header/legacy-categories-tray.php
+++ template-parts/header/legacy-categories-tray.php
@@ -1,7 +1,7 @@
 <div class="legacy-categories-tray fixed">
     <div class="legacy-categories-tray__content">
         <div class="legacy-categories-tray__toggle">
-            <div class="legacy-categories-tray__title">Legacy Categories</div>
+            <div class="legacy-categories-tray__title"><?php echo get_field('categories_tray_toggle', 'option'); ?></div>
         </div>
 
         <div class="legacy-categories-tray__close hidden-desktop">
@@ -9,124 +9,25 @@
         </div>
 
         <div class="legacy-categories-tray__categories">
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/passenger-car-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-157.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/passenger-car-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-magma.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Passenger Car Lubricants', 'cyclon'); ?></h4>
-            </div>
 
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/heavy-duty-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-158.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/heavy-duty-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-granit.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Heavy Duty Lubricants', 'cyclon'); ?></h4>
-            </div>
 
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/motorcycle-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-159.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/motorcycle-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-lava.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Motocycle Lubricants', 'cyclon'); ?></h4>
-            </div>
-
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/gear/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-160.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/gear/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-gear.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Trasmission Lubricants', 'cyclon'); ?></h4>
-            </div>
-
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/industrial-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-161.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/industrial-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-industrial.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Industrial Lubricants', 'cyclon'); ?></h4>
-            </div>
-
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/marine-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-249.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/marine-lubricants/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-marine.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Marine Lubricants', 'cyclon'); ?></h4>
-            </div>
-
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/greases/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/home-greases.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/greases/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-greases.svg">
-                    </a>
-                </div>
-                <h4><?php echo __('Greases', 'cyclon'); ?></h4>
-            </div>
-
-            <div class="catBox legacy-categories-tray__category">
-                <div class="catBox__Inner">
-                    <a href="<?php echo __('/special-products/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/04/Mask-Group-164.png"
-                            class="catBoxImage" />
-                    </a>
-                </div>
-                <div>
-                    <a href="<?php echo __('/special-products/', 'cyclon'); ?>">
-                        <img src="/wp-content/uploads/2022/06/logo-special-fluids.svg">
-                    </a>
-                </div>
-            </div>
+            <?php if (have_rows('categories_tray_list', 'option')): ?>
+                <?php while (have_rows('categories_tray_list', 'option')): the_row(); ?>
+                    <div class="catBox legacy-categories-tray__category">
+                        <div class="catBox__Inner">
+                            <a href="<?php the_sub_field('link'); ?>">
+                                <img src="<?php the_sub_field('image'); ?>" class="catBoxImage" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="<?php the_sub_field('link'); ?>">
+                                <h2><?php the_sub_field('heading'); ?></h2>
+                            </a>
+                        </div>
+                        <h4><?php the_sub_field('subheading'); ?></h4>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
 
         </div>
     </div>
