876208960db95b7e26ef19b5952347b68f660591
vasilisCM <vpafiliaris@conceptmaniax.com>
2026-01-29 14:03:47 +0200

    filters oem specs texts

--- taxonomy-cyclon_new_product_cat.php
+++ taxonomy-cyclon_new_product_cat.php
@@ -139,9 +139,9 @@ get_header(); ?>
                                     // Get placeholder text based on taxonomy
                                     $placeholder = 'All';
                                     if ($taxonomy->name === 'cyclon_new_product_acea') {
-                                        $placeholder = 'Industry Specs';
+                                        $placeholder = 'Industry';
                                     } elseif ($taxonomy->name === 'cyclon_new_product_oem') {
-                                        $placeholder = 'OEM Specs';
+                                        $placeholder = 'OEM';
                                     }
                                 ?>
                                     <!-- Dropdown for ACEA, OEM, and Specifications -->
